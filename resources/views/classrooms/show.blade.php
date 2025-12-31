6ddf27d11e4468e2e15b18af754e11d1c0521fa1
phavanee <phavanee@graduate.utm.my>
2025-12-31 20:32:00 +0800

    add email at add students dropdown

--- resources/views/classrooms/show.blade.php
+++ resources/views/classrooms/show.blade.php
@@ -145,11 +145,16 @@
                                             <ul class="pt-1">
                                                 @foreach($availableStudents as $student)
                                                     <li class="cursor-pointer select-none relative py-2 pl-3 pr-9 text-gray-900 hover:bg-indigo-600 hover:text-white group"
-                                                        x-show="'{{ strtolower($student->full_name) }}'.includes(search.toLowerCase())"
+                                                        x-show="'{{ strtolower($student->full_name) }} {{ strtolower($student->email) }}'.includes(search.toLowerCase())"
                                                         @click="selectedId = '{{ $student->id }}'; selectedName = '{{ $student->full_name }}'; open = false; search = ''">
-                                                        <span class="block truncate font-normal group-hover:font-semibold">
-                                                            {{ $student->full_name }}
-                                                        </span>
+                                                        <div class="flex flex-col">
+                                                            <span class="block truncate font-normal group-hover:font-semibold">
+                                                                {{ $student->full_name }}
+                                                            </span>
+                                                            <span class="block truncate text-gray-500 group-hover:text-indigo-100">
+                                                                {{ $student->email }}
+                                                            </span>
+                                                        </div>
                                                         <span x-show="selectedId === '{{ $student->id }}'" class="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-hover:text-white">
                                                             <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                                                 <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
